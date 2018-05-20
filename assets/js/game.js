00183f961bf0450ac171bf758c91ed0abd563208
djbatt <djbatt@live.com>
2018-05-19 22:17:42 -0400

    genie chooses letter, you can input and get wins now, you can also lose guesses

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,36 +1,10 @@
-var alphabet = {
-    a: 65,
-    b: 66,
-    c: 67,
-    d: 68,
-    e: 69,
-    f: 70,
-    g: 71,
-    72: "h",
-    73: "i",
-    74: "j",
-    75: "k",
-    76: "l",
-    77: "m",
-    78: "n",
-    79: "o",
-    80: "p",
-    81: "q",
-    82: "r",
-    83: "s",
-    84: "t",
-    85: "u",
-    86: "v",
-    87: "w",
-    88: "x",
-    89: "y",
-    90: "z",
-}
+
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 var wins = 0;
 var losses = 0;
-var remainingGuesses = 10;
 var previousGuesses = [];
+var remainingGuesses = 10;
 
 function updatePage() {
     document.getElementById("remainingGuesses").innerText = remainingGuesses;
@@ -39,12 +13,20 @@ function updatePage() {
 }
 
 function newGame() {
-    var selectedLetter = Math.floor(Math.random() * (Object.keys(alphabet).length) + 65);
-    console.log(selectedLetter);
+    var selectedLetter = alphabet[Math.floor(Math.random() * (alphabet.length))];
     updatePage();
-}
-
-document.onkeyup = function(event) {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(userGuess);
+    document.onkeyup = function(event) {
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        if (userGuess === selectedLetter) {
+            wins++;
+            updatePage();
+        }
+        else {
+            if(event.keyCode >= 65 && event.keyCode <=90) { // if you press keys a-z run the script below.
+                remainingGuesses--;
+                updatePage();
+            }
+        }
+    }
+    console.log(selectedLetter);
 }
