22facb00fc18691f86649a691756fb3978f8a171
djbatt <djbatt@live.com>
2018-05-19 22:35:01 -0400

    final touches

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,28 +1,41 @@
 
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
 var wins = 0;
 var losses = 0;
 var previousGuesses = [];
 var remainingGuesses = 10;
 
-function updatePage() {
+function updatePage() { //updates all four page counters when needed
     document.getElementById("remainingGuesses").innerText = remainingGuesses;
+    document.getElementById("previousGuesses").innerText = previousGuesses;
     document.getElementById("wins").innerText = wins;
     document.getElementById("losses").innerText = losses;
 }
 
-function newGame() {
-    var selectedLetter = alphabet[Math.floor(Math.random() * (alphabet.length))];
-    updatePage();
+function newGame() { //the parent code does enough to reset the game and start a new one
+    document.getElementById("remainingGuesses").innerText = remainingGuesses;
+    document.getElementById("wins").innerText = wins;
+    document.getElementById("losses").innerText = losses;
+    remainingGuesses = 10;
+    previousGuesses.length = 0;
+
+    var selectedLetter = alphabet[Math.floor(Math.random() * (alphabet.length))]; //sets the genie's letter
+    
     document.onkeyup = function(event) {
-        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); //sets the user guess variable to whichever key you press
+        previousGuesses.push(userGuess);
         if (userGuess === selectedLetter) {
             wins++;
-            updatePage();
+            newGame();
         }
+
+        else if (remainingGuesses === 0) {
+            losses++;
+            newGame();
+        }
+
         else {
-            if(event.keyCode >= 65 && event.keyCode <=90) { // if you press keys a-z run the script below.
+            if(event.keyCode >= 65 && event.keyCode <=90) { //this makes sure we only changes your remaining guesses if you press a letter key, not a symbol
                 remainingGuesses--;
                 updatePage();
             }
